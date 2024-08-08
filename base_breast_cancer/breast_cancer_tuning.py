307a48e63c6030ab1547de657dc96c28afd47e75
andrebpan <andrebambergp@gmail.com>
2024-08-07 21:57:03 -0300

    1.0

--- base_breast_cancer/breast_cancer_tuning.py
+++ base_breast_cancer/breast_cancer_tuning.py
@@ -10,23 +10,44 @@ from sklearn.model_selection import GridSearchCV
 previsores = pd.read_csv('base_breast_cancer/datasets/entradas_breast.csv')
 classe = pd.read_csv('base_breast_cancer/datasets/saidas_breast.csv')
 
-def criarRede():
+def criarRede(optimizer, loos, kernel_initializer, activation, neurons):
     clf = Sequential()
     #qtdeOculta = (30 + 1) / 2  
-    clf.add(Dense(units = 16, activation = 'relu', kernel_initializer = 'random_uniform', input_dim = 30))
+    clf.add(Dense(units = neurons, activation = activation, kernel_initializer = kernel_initializer, input_dim = 30))
     
     #dropout
     clf.add(Dropout(0.2))
     
-    clf.add(Dense(units = 16, activation = 'relu', kernel_initializer = 'random_uniform'))
+    clf.add(Dense(units = neurons, activation = activation, kernel_initializer = kernel_initializer))
 
     #dropout
     clf.add(Dropout(0.2))
     
     clf.add(Dense(units = 1, activation = 'sigmoid'))
-
-    otimizador = keras.optimizers.Adam(learning_rate = 0.001, decay = 0.0001, clipvalue = 0.5)
-    clf.compile(optimizer = otimizador, loss = 'binary_crossentropy', metrics = ['binary_accuracy'])
+    clf.compile(optimizer = optimizer, loss = loos, metrics = ['binary_accuracy'])
 
     return clf
+
+
+clf = KerasClassifier(build_fn=criarRede)
+
+parametros = {'batch_size':[10, 30],
+              'epochs':[50, 100], 
+              'model__optimizer':['adam', 'sgd'],
+              'model__loos':['binary_crossentropy', 'hinge'], 
+              'model__kernel_initializer':['random_uniform', 'normal'],
+              'model__activation':['relu', 'tanh'],
+              'model__neurons':[16, 8]}
+
+grid_search = GridSearchCV(estimator=clf,
+                           param_grid=parametros,
+                           scoring='accuracy',
+                           cv=5)
+
+grid_search = grid_search.fit(previsores, classe)
+melhores_parametros = grid_search.best_params_
+melhor_precisao = grid_search.best_score_
+
+print("\nMelhores parâmetros: ", melhores_parametros)
+print("\nMelhor precisão: ", melhor_precisao)
 
